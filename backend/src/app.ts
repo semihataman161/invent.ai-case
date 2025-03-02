589615ac3767694b2875c8802ae45f23ce995411
semihataman161 <semihataman16@gmail.com>
2025-03-02 17:50:42 +0300

    Fix cors errors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import UserRoutes from "./routes/user/user.route";
 import BookRoutes from "./routes/book/book.route";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(UserRoutes);
 app.use(BookRoutes);
